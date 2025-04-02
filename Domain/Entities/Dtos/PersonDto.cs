fe57c15c7e6d099c6192aac3700559e976a17f6a
Bruno Silva <brunovinicius0704@gmail.com>
2025-04-02 20:26:57 -0300

    feat: create main entity

---
+++ Domain/Entities/Dtos/PersonDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities.Dtos
+{
+    public class PersonDto(Person person)
+    {
+        public Guid Id { get; set; } = person.Id;
+        public string Name { get; set; } = person.Name;
+        public string Email { get; set; } = person.Email;
+        public bool Active { get; set; } = person.Active;
+
+        public static implicit operator PersonDto(Person person)
+            => new PersonDto(person);
+    }
+}
---
+++ Domain/Entities/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace Domain.Entities
+{
+    public class Person : BaseEntity
+    {
+        protected Person() { }
+        private Person(string name, string email, bool active, int age, string address, string otherInformation, string interests, string feelings, string values)
+        {
+            Name = name;
+            Email = email;
+            Active = active;
+            Age = age;
+            Address = address;
+            OtherInformation = otherInformation;
+            Interests = interests;
+            Feelings = feelings;
+            Values = values;
+        }
+
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public bool Active { get; set; }
+        public int? Age { get; set; }
+        public string? Address { get; set; }
+        public string? OtherInformation { get; set; }
+        public string? Interests { get; set; }
+        public string? Feelings { get; set; }
+        public string? Values { get; set; }
+
+        public static class Factory
+        {
+            public static Person Create(string name, string email, bool active, int age, string address, string otherInformation, string interests, string feelings, string values)
+                => new Person(name, email, active, age, address, otherInformation, interests, feelings, values);
+        }
+
+    }
+}
