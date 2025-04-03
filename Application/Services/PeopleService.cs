cc82b368bc2f45c8667320787e579abe9be65e2e
Bruno Silva <brunovinicius0704@gmail.com>
2025-04-02 21:54:02 -0300

    feat: list people

--- Application/Services/PeopleService.cs
+++ Application/Services/PeopleService.cs
@@ -6,25 +6,29 @@ using Domain.SeedWork.Notification;
 using Domain.Services;
 using Domain.Services.Requests;
 using Domain.Services.Responses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace Application.Services;
 
-public class PeopleService(INotification notification, IPeopleRepository repository) : BaseService(notification), IPeopleService
+public class PeopleService(INotification notification, IPeopleRepository repository, IDistributedCache cache) : BaseService(notification), IPeopleService
 {
     public Task<BaseResponse<HomeScoreResponse>> GetHomeScoreAsync()
     {
         throw new NotImplementedException();
     }
 
-    public Task<BaseResponse<List<PersonDto>>> ListPersonAsync()
+    public async Task<BaseResponse<List<PersonDto>>> ListPersonAsync()
     {
-        throw new NotImplementedException();
+        var list = await repository.GetAll().Select(x => (PersonDto)x).ToListAsync();
+        return new GenericResponse<List<PersonDto>>(list);
     }
 
     public async Task<BaseResponse<PersonDto>> RegisterPersonAsync(RegisterPersonRequest request)
     {
         var person = Person.Factory.Create(request, notification);
         await repository.InsertWithSaveChangesAsync(person);
+        cache.Remove("GET:/api/people");
         return new GenericResponse<PersonDto>(person);
     }
 }
